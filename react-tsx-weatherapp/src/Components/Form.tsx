2088eef5fced7eecca1cfb42761ff7640bebfc17
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-09-29 10:40:00 -0400

    created dropdown for country code

--- react-tsx-weatherapp/src/Components/Form.tsx
+++ react-tsx-weatherapp/src/Components/Form.tsx
@@ -1,7 +1,12 @@
 import React, { useState, ChangeEvent } from 'react';
+import { countryCodes } from '../countryCodes';
 
 let zipcodeValue: string;
 let setZipcodeValue: Function;
+
+let countryCode: string;
+let setCountryCode: Function;
+
 interface FormProps {
 	handleSubmit: Function;
 }
@@ -11,20 +16,39 @@ const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
 	setZipcodeValue(event.target.value);
 };
 
+const handleCountryCode = (event: ChangeEvent<HTMLSelectElement>) => {
+	console.log(event.target.value);
+	setCountryCode(event.target.value);
+};
+
 export const Form = (props: FormProps) => {
 	[zipcodeValue, setZipcodeValue] = useState('');
+	[countryCode, setCountryCode] = useState('');
 
 	const handleSubmit = (e: any) => {
-		props.handleSubmit(zipcodeValue);
+		props.handleSubmit(zipcodeValue, countryCode);
 		e.preventDefault();
 	};
 
+	const countryOptions = countryCodes.map((code, index) => {
+		return <option value={code}>{code}</option>;
+	});
+
 	// could add an dropdown here to handle country code
 	return (
-		<form action='#' onSubmit={handleSubmit}>
-			<label htmlFor='zipcode'>Enter zipcode: </label>
-			<input onChange={(e) => handleChange(e)} type='text' name='zipcode' />
-			<input type='submit' />
-		</form>
+		<>
+			<form action='#' onSubmit={handleSubmit}>
+				<label htmlFor='zipcode'>Enter zipcode: </label>
+				<input onChange={(e) => handleChange(e)} type='text' name='zipcode' />
+				<label htmlFor='country'>Select country</label>
+				<select name='country' id='countries' onChange={handleCountryCode}>
+					{countryOptions}
+				</select>
+				<input type='submit' />
+			</form>
+			<a target='#' href='https://www.iso.org/obp/ui/#search'>
+				What's my country code?
+			</a>
+		</>
 	);
 };
