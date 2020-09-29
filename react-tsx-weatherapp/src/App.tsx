d08fe40335187a12a93d30c0c5219bb5294ec6dc
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-09-29 09:03:02 -0400

    basic functionality working for US country codes

--- react-tsx-weatherapp/src/App.tsx
+++ react-tsx-weatherapp/src/App.tsx
@@ -1,34 +1,91 @@
 import React, { useState, SyntheticEvent } from 'react';
-import logo from './logo.svg';
 import { Form } from './Components/Form';
+import { WeatherCard } from './Components/WeatherCard';
 import './App.css';
 
 // interfaces & types
-
-const handleSubmit = async (zip: string) => {
-	console.log('weathurrrgh');
-
-	console.log(zip);
-
-	let openWeatherAPI: string = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=8f02608638b1891744e6b2750a862506`;
-
-	async function apiCall(request: RequestInfo): Promise<any> {
-		const response = await fetch(request);
-		const body = await response.json();
-		return body;
-	}
-
-	// example consuming code
-	const data = await apiCall(openWeatherAPI);
-	console.log(data);
-};
+interface IPlaceProps {
+	city?: string;
+	// country?: string;
+	temp?: number;
+	temp_min?: number;
+	temp_max?: number;
+	description?: string;
+}
 
 function App() {
+	const [place, setPlace] = useState<IPlaceProps>({
+		city: 'string',
+		// country: 'string',
+		temp: 0,
+		temp_min: 0,
+		temp_max: 0,
+		description: 'string',
+	});
+	const [searched, setSearched] = useState(false);
+
+	// HANDLE FORM SUBMIT and pass inputted zipcode to API
+	const handleSubmit = async (zip: string) => {
+		console.log(zip);
+
+		// FETCH DATA FROM API with .then and .catch -- I don't know how to use async/await to handle errors in TS, and barely understand how to do it in JS
+		async function apiCall(): Promise<any> {
+			let openWeatherAPI: string = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=8f02608638b1891744e6b2750a862506`;
+
+			try {
+				const response = await fetch(openWeatherAPI);
+				const body = await response.json();
+				return body;
+			} catch (err) {
+				if (err) {
+					console.log(err.message);
+				}
+			}
+		}
+
+		// FETCH DATA FROM API with .then and .catch
+		// async function apiCall(): Promise<any> {
+		// 	let openWeatherAPI: string = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=8f02608638b1891744e6b2750a862506`;
+
+		// 		const response = await fetch(openWeatherAPI);
+		// 		const body = await response.json();
+		// 		return body;
+
+		// 	}
+		// }
+
+		// CONSUME API CALL
+
+		const data = await apiCall();
+		// console.log(data.sys.country);
+
+		setSearched(true);
+		setPlace({
+			city: data.name,
+			// country: data.sys.country,
+			temp: data.main.temp,
+			temp_min: data.main.temp_min,
+			temp_max: data.main.temp_max,
+			description: data.weather.description,
+		});
+	};
+
+	const card =
+		searched === true ? (
+			<WeatherCard
+				city={place.city}
+				temp={place.temp}
+				temp_min={place.temp_min}
+				temp_max={place.temp_max}
+				description={place.description}
+			/>
+		) : null;
+
 	return (
 		<div className='App'>
 			<h1>What's the weather like?</h1>
 			<Form handleSubmit={handleSubmit} />
-			<WeatherCard />
+			{card}
 		</div>
 	);
 }
