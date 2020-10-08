a94824768df1082c6e1375335ca707759cfea6fa
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-08 11:08:27 -0400

    Updated <title>

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,5 +1,5 @@
-import React, { useState, ChangeEvent } from 'react';
-import { countryCodes } from '../countryCodes';
+import React, { useState, ChangeEvent } from "react";
+import { countryCodes } from "../countryCodes";
 
 let zipcodeValue: string;
 let setZipcodeValue: Function;
@@ -8,47 +8,56 @@ let countryCode: string;
 let setCountryCode: Function;
 
 interface FormProps {
-	handleSubmit: Function;
+  handleSubmit: Function;
 }
 
 const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-	setZipcodeValue(event.target.value);
+  setZipcodeValue(event.target.value);
 };
 
 const handleCountryCode = (event: ChangeEvent<HTMLSelectElement>) => {
-	setCountryCode(event.target.value);
+  setCountryCode(event.target.value);
 };
 
 export const Form = (props: FormProps) => {
-	[zipcodeValue, setZipcodeValue] = useState('');
-	[countryCode, setCountryCode] = useState('');
+  [zipcodeValue, setZipcodeValue] = useState("");
+  [countryCode, setCountryCode] = useState("");
 
-	const handleSubmit = (e: any) => {
-		props.handleSubmit(zipcodeValue, countryCode);
-		e.preventDefault();
-	};
+  // change this type to be more descriptive if possible
+  const handleSubmit = (e: any) => {
+    props.handleSubmit(zipcodeValue, countryCode);
+    e.preventDefault();
+  };
 
-	const countryOptions = countryCodes.map((code, index) => {
-		return <option value={code}>{code}</option>;
-	});
+  const countryOptions = countryCodes.map((code, index) => {
+    return (
+      <option key={index} value={code}>
+        {code}
+      </option>
+    );
+  });
 
-	return (
-		<>
-			<form action='#' onSubmit={handleSubmit}>
-				<label htmlFor='zipcode'>Enter zipcode: </label>
-				<input onChange={(e) => handleChange(e)} type='text' name='zipcode' />
-				<label htmlFor='country'>Select country</label>
-				<select
-					name='country'
-					id='countries'
-					onChange={(e) => handleCountryCode(e)}>
-					{countryOptions}
-				</select>
-				<input type='submit' />
-			</form>
-			<a target='#' href='https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes'>
-				What's my country code?
-			</a>
-		</>
-	);
+  return (
+    <>
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="zipcode">Enter zipcode: </label>
+        <input onChange={(e) => handleChange(e)} type="text" name="zipcode" />
+        <label htmlFor="country">Select country</label>
+        <select
+          name="country"
+          id="countries"
+          onChange={(e) => handleCountryCode(e)}
+        >
+          {countryOptions}
+        </select>
+        <input type="submit" />
+      </form>
+      <a
+        target="#"
+        href="https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes"
+      >
+        What's my country code?
+      </a>
+    </>
+  );
 };
--- src/Components/WeatherCard.tsx
+++ src/Components/WeatherCard.tsx
@@ -1,43 +1,42 @@
-import React from 'react';
+import React from "react";
 
 interface WeatherCardProps {
-	city?: string;
-	// country?: string;
-	temp?: number;
-	temp_min?: number;
-	temp_max?: number;
-	description?: string;
+  city: string;
+  temp: number;
+  temp_min: number;
+  temp_max: number;
+  description: string;
 }
 
 export const WeatherCard = (props: WeatherCardProps) => {
-	let changeColor;
+  let changeColor;
 
-	if (props.temp && props.temp >= 90) {
-		changeColor = 'red';
-	}
+  if (props.temp && props.temp >= 90) {
+    changeColor = "red";
+  }
 
-	if (props.temp && props.temp <= 40) {
-		changeColor = 'blue';
-	}
+  if (props.temp && props.temp <= 40) {
+    changeColor = "blue";
+  }
 
-	return (
-		<div className='weather-card'>
-			<p>{props.city}</p>
-			{/* <p>{props.country}</p> */}
-			<h1 className={`${changeColor}`}>{props.temp}</h1>
-			<h3>
-				<em>{props.description}</em>
-			</h3>
-			<div className='min-max'>
-				<div className='min-or-max'>
-					<h4>Min:</h4>
-					<p>{props.temp_min}</p>
-				</div>
-				<div className='min-or-max'>
-					<h4>Max:</h4>
-					<p>{props.temp_max}</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="weather-card">
+      <p>{props.city}</p>
+      {/* <p>{props.country}</p> */}
+      <h1 className={`${changeColor}`}>{props.temp}</h1>
+      <h3>
+        <em>{props.description}</em>
+      </h3>
+      <div className="min-max">
+        <div className="min-or-max">
+          <h4>Min:</h4>
+          <p>{props.temp_min}</p>
+        </div>
+        <div className="min-or-max">
+          <h4>Max:</h4>
+          <p>{props.temp_max}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
